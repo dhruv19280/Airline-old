bae815d3123acf3f50ca7d13094c42f403db9bc8
Dhruv Mathur <dhruv19280@gmail.com>
2020-01-29 18:55:08 +0530

    none

---
+++ model/Airport.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Airport {
+    private String sAirportName;
+    private String sAirportICAO;
+    private String sAirportSize;
+    private String sAirportType;
+    private String sAirportCity;
+
+    private Double dLatitucdde;
+    private Double dLongitude;
+
+    private Integer iPassengerCapacity;
+    private Integer iCargoCapacity;
+
+    private Integer iWaitingPassengers;
+    private Integer iWaitingCargo;
+
+    
+
+}
---
+++ model/GameInitializer.java
@@ -0,0 +1,8 @@
+package model;
+
+public class GameInitializer() {
+
+    public static void Initalize() {
+        
+    }
+}
