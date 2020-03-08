8f90740de15265575a27ab95e8f243b6066c9813
Dhruv Mathur <dhruv19280@gmail.com>
2020-03-08 16:06:56 +0530

    none

--- Airline/src/model/AirlineTemplate.java
+++ Airline/src/model/AirlineTemplate.java
@@ -2,15 +2,40 @@ package Airline.src.model;
 
 public interface AirlineTemplate {
 
+// Route Operations
     public void OpenRoute();
     public void CloseRoute();
+    public void AuditRoute();
+    public void PayRouteRents();
+    public void PromoteRoute();
+    public void SetEconomyFare();
+    public void SetBusinessFare();
+    public void SetFirstFare();
+    public void SetCargoFare();
+
+
+// Aircraft Operations
     public void PurchaseAircraft();
-    public void RetireAircraft();
+    public void DeployAircraft();
+    public void UpgradeAircraft();
     public void MaintainAircraft();
+    public void RetireAircraft();
+
+
+// Crew Operations
     public void HireCrew();
-    public void FireCrew();
+    public void TrainCrew();
+    public void AssignCrew();
     public void PaySalaries();
+    public void FireCrew();
+
+//Airport Operations
     public void OpenBranch();
     public void CloseBranch();
     public void PayBranchRents();
+
+
+// Flight Schedule Operations
+    public void ScheduleFlights();
+    public void DescheduleFlights();
 }
--- Airline/src/model/Crew.java
+++ Airline/src/model/Crew.java
@@ -35,3 +35,118 @@ enum EmployeeType {
     AirHostess,
     Management
 }
+
+/*
+
+Most Common Male First Names of the World
+
+James
+John
+Robert
+Michael
+William
+David
+Richard
+Joseph
+Thomas
+Charles
+Christopher
+Daniel
+Matthew
+Anthony
+Donald
+Mark
+Paul
+Steven
+Andrew
+Kenneth
+
+Most Common Female First Names of the World
+
+Mary
+Patricia
+Jennifer
+Linda
+Elizabeth
+Barbara
+Susan
+Jessica
+Sarah
+Karen
+Nancy
+Margaret
+Lisa
+Betty
+Dorothy
+Sandra
+Ashley
+Kimberly
+Donna
+Emily
+
+
+Most Common Surnames of the World!
+Wang
+Devi
+Zhang
+Li
+Liu
+Singh
+Yang
+Kumar
+Wu
+Xu
+Smith
+Johnson
+Williams
+Brown
+Jones
+Miller
+Davis
+Wilson
+Anderson
+Taylor
+Latu
+Gonzales
+Rodriguez
+Hernandez
+Murphy
+Perera
+Nguyen
+Nkosi
+Ahmad
+Ali
+Phiri
+Ibrahim
+Moyo
+Tesfaye
+Sato
+Hansen
+Garcia
+Silva
+Ivanov
+Rossi
+Nowak
+Williams
+
+
+
+
+1. James Smith - 38,313
+2. Michael Smith - 34,810
+3. Robert Smith - 34,269
+4. Maria Garcia - 32,092
+5. David Smith - 31,294
+6. Maria Rodriguez - 30,507
+7. Mary Smith - 28,692
+8. Maria Hernandez - 27,836
+9. Maria Martinez - 26,956
+10. James Johnson - 26,850
+11. William Smith - 26,074
+12. Robert Johnson - 28,874
+13. John Smith - 25,255
+
+
+
+
+ */
