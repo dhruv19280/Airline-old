ccdee8a2d63460dd96348b52e2ca2f15f0b1e14a
Dhruv Mathur <dhruv19280@gmail.com>
2020-03-24 13:46:41 +0530

    none

---
+++ Airline/src/model/AirlineGame.java
@@ -0,0 +1,35 @@
+package Airline.src.model;
+
+public class AirlineGame {
+
+    public static void Initialize() {
+
+        System.out.println("Initializing Airports...");
+        AirportsInitializer.InitializeAll();
+        //AirportsInitializer.PrintAllAirports();
+        System.out.println("Airports Initialization Completed.");
+
+        System.out.println("Initializing Aircrafts...");
+        AircraftModelsInitializer.InitializeAll();
+        AircraftModelsInitializer.PrintAllAircraftModels();
+        System.out.println("Aircrafts Initialization Completed.");
+
+        System.out.println("Initializing Airlines...");
+        AirlineInitializer.InitializeAll();
+        AirlineInitializer.PrintAllAirlines();
+        System.out.println("Airlines Initialization Completed.");
+
+        System.out.println("Initializing GameTime...");
+        GameTime gtGameTime = new GameTime();
+        Thread thGameTime = new Thread(gtGameTime);
+        thGameTime.start();
+        System.out.println("GameTime Initialized and Running.");
+
+        /*
+        AirlinesInitializer.InitializeAll();
+        GameGenerator.InitializeAll();
+        CrewInitializer.InitializeAll();
+        RouteInitializer.InitializeAll();
+        */
+    }
+}
